ae64fbaaa2982f4c13e3a15c3a0b443b888182e4
Dan Johnson <danimal.johnson@gmail.com>
2023-07-14 10:19:47 -0700

    Add favicon

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import 'dotenv/config';
 import express, { Request, Response, NextFunction } from 'express';
 import path from 'path';
 import publicRouter from './data/routers/publicRouter';
+const favicon = require('serve-favicon');
 
 const server: express.Application = express();
 
 // Middleware
 server.use(express.json());
 server.use(express.static(path.join(__dirname, 'public')));
+server.use(favicon(path.join(__dirname, 'public/public', 'favicon.ico')));
 
 // Routes
 
