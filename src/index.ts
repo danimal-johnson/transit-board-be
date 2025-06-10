e184486a1c65553a50aca8b6c18baf924e713135
Dan Johnson <danimal.johnson@gmail.com>
2025-06-09 17:00:32 -0700

    Enable CORS for frontend data access.

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import 'dotenv/config';
 import express, { Request, Response, NextFunction } from 'express';
 import path from 'path';
 import publicRouter from './data/routers/publicRouter';
+import cors from 'cors';
 const favicon = require('serve-favicon');
 
 const server: express.Application = express();
 
 // Middleware
 server.use(express.json());
+server.use(cors());
 server.use(express.static(path.join(__dirname, 'public')));
 server.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
